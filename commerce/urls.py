1cc363057b4652df552c061ac0a7f02ec868542f
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-05 09:30:43 +0530

    Made changes

--- commerce/urls.py
+++ commerce/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.static import serve
 from django.conf.urls import url
+from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls")),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
