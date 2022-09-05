4fc0266467c291b82e7bafad013c513bf1428b9f
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-05 09:33:28 +0530

    Made changes

--- commerce/urls.py
+++ commerce/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_URL}),
 ]
