e3e0b3388da45d0e9924a8ff318f52b55a4d8d97
Vaibhav <rawatvaibhav42@gmail.com>
2022-09-05 09:18:45 +0530

    hosting edits

--- commerce/urls.py
+++ commerce/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("auctions.urls"))
+    path("", include("auctions.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
